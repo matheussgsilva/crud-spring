de3f3b817bffe512844b5e3b4e70e249f28415ab
Matheussgsilva <matheussgsilva@hotmail.com>
2022-10-17 19:21:22 -0300

    Create the spring project and create course controller and course model

---
+++ src/main/java/com/matheus/crudspring/controller/CourseController.java
@@ -0,0 +1,19 @@
+package com.matheus.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.matheus.model.Course;
+
+@RestController
+@RequestMapping("/api/courses")
+public class CourseController {
+  
+  @GetMapping
+  public List<Course> list() {
+    return null;
+  }
+}
